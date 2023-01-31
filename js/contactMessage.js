33b1d74b02f833f6e1464e550fe7ae7a5efd0a7b
KThomiss <79108090+KThomiss@users.noreply.github.com>
2023-01-31 20:26:36 +0100

    update

--- js/contactMessage.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("form");
-const messageContainer = document.querySelector(".message-container");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  messageContainer.classList.add("message");
-});
