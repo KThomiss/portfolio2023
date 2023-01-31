5f1a3dfd4248e0c3d7af8ebbf1c36056647ca16c
KThomiss <79108090+KThomiss@users.noreply.github.com>
2023-01-31 21:12:01 +0100

    animation

--- js/textAnimation.js
+++ js/textAnimation.js
@@ -1,7 +1,13 @@
 const heading = document.querySelector("h1");
-const text = heading.innerText.split("");
+const title = heading.innerText.split("");
 heading.innerText = "";
+for (let i = 0; i < title.length; i++) {
+  heading.innerHTML += `<span data-id="${[i]}">${title[i]}</span>`;
+}
 
-for (let i = 0; i < text.length; i++) {
-  heading.innerHTML += `<span data-id="${[i]}">${text[i]}</span>`;
+const subHeading = document.querySelector(".intro-span-text");
+const statement = subHeading.innerText.split(" ");
+subHeading.innerText = "";
+for (let j = 0; j < statement.length; j++) {
+  subHeading.innerHTML += `<span data-num="${[j]}">${statement[j]}</span>`;
 }
